352ef2fcf8c74a224a0d414d8f425d2f5fb3fedb
Robson Edwards <robson.edwards@gmail.com>
2020-04-02 14:48:08 -0700

    Add check for duplicates

--- vrd.py
+++ vrd.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 from collections import defaultdict
+from levenshtein import levenshtein
 
 # Load VRD data
 paths = os.listdir("data")
@@ -31,6 +32,19 @@ for df in dfs:
 cards_list = []
 for card, ls in cards.items():
     cards_list.append([card, min(ls) + 1, round(sum(ls) / len(ls) + 1, 2)])
+
+# Check for misspellings/duplicates
+CHECK_FOR_DUPLICATES = False
+if(CHECK_FOR_DUPLICATES):
+    for i in range(len(cards_list)):
+        for j in range(i + 1, len(cards_list)):
+            card1 = cards_list[i][0]
+            card2 = cards_list[j][0]
+            d = levenshtein(card1, card2)
+            if d < 3:
+                print("".join(["Warning! Cards ", card1, " and ",
+                    card2, " may be misspellings of the same card!"]))
+
 # Sort by min pick #
 cards_list = sorted(cards_list, key = lambda x : x[1])
 # then sort by average pick #
